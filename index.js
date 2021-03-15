dcba31edc9a4087b197c3fd21b686c8a834989a5
Michael <mawwoodley@gmail.com>
2021-03-14 22:57:02 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(x,y){
+    return x + y;
+}
+
+function subtract(x,y){
+    return x - y;
+
+}
+function multiply(x,y){
+    return x * y;
+    
+}
+function divide(x,y){
+    return x / y;
+
+}
+ function increment(x){
+     x++;
+     return x++;
+ }
+
+ function decrement(x){
+    x--; 
+    return x--;
+ }
+
+ function makeInt(x,base){
+    parseInt('x',10);
+   return (parseInt(x,10))
+ }
+
+ function preserveDecimal(x){
+     return (parseFloat(x));
+   
+ }
+ 
